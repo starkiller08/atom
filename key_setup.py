5ad18427f7f51e893316a227f29062bc36a2b8cb
Sarim Khan <sarimkhan752@gmail.com>
2026-08-28 17:48:11 +1000

    Fixed issues with the password checking logic to make it more robust by identify illegal character usage even if it satisfies the character length requirement. Added more comments to explain more parts of the code clearly.

--- key_setup.py
+++ key_setup.py
@@ -16,6 +16,9 @@ alice_public_key = alice_private_key.publickey()
 alice_public_key_bytes = alice_public_key.export_key()
 
 
+# Save public and private keys inside the Alice folder
+
+os.makedirs('Alice', exist_ok=True)
 with open('Alice/alice_private_key.pem', 'wb') as f:
 	print('Saving file "alice_private_key.pem" to folder "Alice"')
 	f.write(alice_private_key_bytes)
@@ -28,23 +31,28 @@ with open('Alice/alice_public_key.pem', 'wb') as f:
 
 alice_public_key_hash = hl.sha1(alice_public_key_bytes).hexdigest()
 
+# Save Alice's public key fingerprint in Bob's folder
+
+os.makedirs('Bob', exist_ok=True)
 with open('Bob/alice_public_key_fingerprint.txt', 'w') as f:
 	print('Saving file "alice_public_key_fingerprint" to folder "Bob"')
 	f.write(alice_public_key_hash)
 
 
+# Password length and character checking logic
 while True:
 	password = input('Please enter your password (8 alphanumeric characters exactly): ')
-	if len(password) == 8 and password.isalnum():
+	if len(password.encode()) == 8 and password.isalnum() == True:
 		break
-
-	elif len(password) < 8:
+	elif len(password.encode()) == 8 and password.isalnum() == False:
+		print('Error!! Password must be letters and numbers only.')
+	elif len(password.encode()) < 8:
 		print(f'Password too short. Add {8 - len(password)} characters.')
-	elif len(password) > 8:
+	elif len(password.encode()) > 8:
 		print(f'Password too long. Remove {len(password) - 8} characters.')
 
 pass_hash = hl.sha1(password.encode()).hexdigest()
 
 with open('Alice/passwords.txt', 'w') as f:
-	print('Saving file "passwords.txt" to folder Alice')
+	print('Saving file "passwords.txt" to folder "Alice"')
 	f.write(f'bob:{pass_hash}')
