3c11585d25d8ebbfe3167a815806a20e084a0aff
Sarim Khan <sarimkhan752@gmail.com>
2026-08-28 21:22:14 +1000

    Implemented proper RSA encryption round trip verification functionality. Added a .gitignore file.

--- Alice/host.py
+++ Alice/host.py
@@ -1,4 +1,12 @@
 import socket as s
+import os
+import json as j
+import base64 as b64
+import hashlib as hl
+from Crypto.PublicKey import RSA as rsa
+from Crypto.Cipher import PKCS1_OAEP as pkcs1_oaep
+
+
 
 sock = s.socket(s.AF_INET, s.SOCK_DGRAM)
 sock.bind(('127.0.0.1', 1111))
@@ -7,4 +15,31 @@ print('Host listening on port 1111....')
 data, addr = sock.recvfrom(4096)
 print(f'Connection request received... {data} {addr}')
 
-sock.sendto(b'got it', addr)
+
+m = j.loads(data.decode())
+nb = b64.b64decode(m['nb'])
+print(f'User: {m["user"]}, NB length: {len(nb)}')
+
+
+pk_bytes = open('Alice/alice_public_key.pem','rb').read()
+na = os.urandom(16)
+reply = j.dumps({'msg':2,'user':'alice',
+                 'pk': b64.b64encode(pk_bytes).decode(),
+                 'na': b64.b64encode(na).decode()}).encode()
+sock.sendto(reply, addr)
+
+
+
+data, addr = sock.recvfrom(4096)
+m3 = j.loads(data.decode())
+c1 = b64.b64decode(m3['c1'])
+sk = rsa.import_key(open('Alice/alice_private_key.pem','rb').read())
+blob = pkcs1_oaep.new(sk).decrypt(c1)
+pw_recv, k = blob[:8], blob[8:]
+
+
+rec = open('Alice/passwords.txt').read().strip().split(':')
+if hl.sha1(pw_recv).hexdigest() == rec[1]:
+    print('Password verified')
+else:
+    print('Password incorrect')
--- Bob/client.py
+++ Bob/client.py
@@ -1,11 +1,36 @@
 import socket as s
+import os
+import json as j
+import base64 as b64
+import hashlib as hl
+from Crypto.PublicKey import RSA as rsa
+from Crypto.Cipher import PKCS1_OAEP as pkcs1_oaep
+
 
 sock = s.socket(s.AF_INET, s.SOCK_DGRAM)
 sock.bind(('127.0.0.1', 2222))
 
-sock.sendto(b'Hello', ('127.0.0.1', 1111))
+nb = os.urandom(16)
+sock.sendto(j.dumps({'msg':1,'user':input('Username: '),'nb':b64.b64encode(nb).decode()}).encode(), ('127.0.0.1', 1111))
 print('Connection request sent...')
 
 data, addr = sock.recvfrom(4096)
 
 print(f'Data sent.... {data} {addr}')
+
+
+m2 = j.loads(data.decode())
+pk_bytes = b64.b64decode(m2['pk'])
+na = b64.b64decode(m2['na'])
+fp = open('Bob/alice_public_key_fingerprint.txt').read().strip()
+if hl.sha1(pk_bytes).hexdigest() != fp:
+    print('Fingerprint mismatch - terminating')
+    exit()
+print('Host public key verified')
+
+
+pw = input('Password: ')
+k = os.urandom(16)
+cipher = pkcs1_oaep.new(rsa.import_key(pk_bytes))
+c1 = cipher.encrypt(pw.encode() + k)
+sock.sendto(j.dumps({'msg':3,'c1':b64.b64encode(c1).decode()}).encode(), ('127.0.0.1',1111))
